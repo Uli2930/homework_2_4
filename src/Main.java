29d5df4fe9134a5e7b540ebe9eb43ca01f5a5cf0
Amin_stors <uli2930tong@gmail.com>
2024-01-17 20:02:36 +0300

    hww4

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<String> namesA = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите имя: " + (i + 1));
+            String name = scan.nextLine();
+
+            namesA.add(name);
         }
+        System.out.println("Ваш список А " + namesA);
+        System.out.println("----------");
+
+        ArrayList<String> namesB = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите имя: " + (i + 1));
+            String name = scan.nextLine();
+
+            namesB.add(name);
+        }
+        System.out.println("Ваш список В " + namesB);
+        System.out.println("----------");
+
+        ArrayList<String> nameC = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            nameC.add(namesA.get(i));
+            nameC.add(namesB.get(4 - i));
+        }
+        System.out.println("Ваш список C " + nameC);
+
+        nameC.sort(Comparator.comparingInt(String::length));
+        System.out.println(nameC);
     }
 }
